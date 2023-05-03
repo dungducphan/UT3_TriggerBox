bc931a825c5bf2b628583393fab89be0208c54cb
dungducphan <brianp.dung@utexas.edu>
2023-05-03 11:52:00 -0500

    Adding more channels and timer controls to the CHICKEN BOX.

--- project0.c
+++ project0.c
@@ -21,12 +21,17 @@
 // Global variables
 //*****************************************************************************
 // Delay values in ms
-volatile uint32_t g_ui32DelayShutter;
-volatile uint32_t g_ui32DelayGasJet;
-volatile uint32_t g_ui32DelayShotCameras;
+volatile uint32_t g_ui32Delay0;
+volatile uint32_t g_ui32Delay1;
+volatile uint32_t g_ui32Delay2;
+volatile uint32_t g_ui32Delay3;
+volatile uint32_t g_ui32Delay4;
+volatile uint32_t g_ui32Delay5;
 // UART command array
 volatile uint32_t g_ui32UARTCommand[COMMAND_LENGTH];
 volatile uint32_t g_ui32UARTCommandIndex;
+// State
+volatile bool     g_ui32RunningState;
 
 //*****************************************************************************
 // Reset UART Commands
@@ -36,14 +41,6 @@ void ResetUARTCommand() {
     for (i = 0; i < COMMAND_LENGTH; i++) g_ui32UARTCommand[i] = 0;
     g_ui32UARTCommandIndex = 0;
 }
-
-//*****************************************************************************
-// The error routine that is called if the driver library encounters an error.
-//*****************************************************************************
-#ifdef DEBUG
-void
-__error__(char *pcFilename, uint32_t ui32Line) {}
-#endif
 
 //*****************************************************************************
 // Setting up the system clock
@@ -57,10 +54,19 @@ void ConfigureSystemClock() {
 // Restart TIMER
 //*****************************************************************************
 void StartTimer(void) {
+    MAP_TimerLoadSet(TIMER0_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32Delay0);
+    MAP_TimerLoadSet(TIMER1_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32Delay1);
+    MAP_TimerLoadSet(TIMER2_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32Delay2);
+    MAP_TimerLoadSet(TIMER3_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32Delay3);
+    MAP_TimerLoadSet(TIMER4_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32Delay4);
+    MAP_TimerLoadSet(TIMER5_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32Delay5);
     // Enable the timers.
     MAP_TimerEnable(TIMER0_BASE, TIMER_A);
     MAP_TimerEnable(TIMER1_BASE, TIMER_A);
     MAP_TimerEnable(TIMER2_BASE, TIMER_A);
+    MAP_TimerEnable(TIMER3_BASE, TIMER_A);
+    MAP_TimerEnable(TIMER4_BASE, TIMER_A);
+    MAP_TimerEnable(TIMER5_BASE, TIMER_A);
 }
 
 //*****************************************************************************
@@ -75,12 +81,16 @@ void PortEIntHandler(void) {
     }
 
     MAP_GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5, 0x0);
+    MAP_GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6, 0x0);
+    MAP_GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x0);
 }
 
 //*****************************************************************************
-// Setting up the IO pins on port E
+// Setting up the IO pins
 //*****************************************************************************
-void ConfigureIO_PortE() {
+void ConfigureIOPorts() {
+//////// Port E
+
     // Enable and wait for the port to be ready for access
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)) {}
@@ -97,26 +107,36 @@ void ConfigureIO_PortE() {
     MAP_GPIOIntTypeSet(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_RISING_EDGE);
     MAP_GPIOIntRegister(GPIO_PORTE_BASE, &PortEIntHandler);
     MAP_GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_1);
+
+//////// Port C
+
+    // Enable and wait for the port to be ready for access
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC)) {}
+
+    // Configure the pins C4, C5, C6 to be the output
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6);
+
+//////// Port D
+
+    // Enable and wait for the port to be ready for access
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)) {}
+
+    // Configure the pins D0, D1, D2, D3 to be the output
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 }
 
 //*****************************************************************************
-// Setting up the IO pins on port F
-//*****************************************************************************
-void ConfigureIO_PortF() {
-    // Enable the GPIO port that is used for the on-board LED.
-    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-
-    // Enable the GPIO pins for the LED (PF2).
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-}
-
-//*************************************************************************************************************************************
 //  Configure Periodic Timer
-//*************************************************************************************************************************************
+//*****************************************************************************
 void ConfigureTimer(void) {
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER2);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER3);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER4);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER5);
 
     // Enable processor interrupts.
     MAP_IntMasterEnable();
@@ -125,17 +145,23 @@ void ConfigureTimer(void) {
     MAP_TimerConfigure(TIMER0_BASE, TIMER_CFG_ONE_SHOT);
     MAP_TimerConfigure(TIMER1_BASE, TIMER_CFG_ONE_SHOT);
     MAP_TimerConfigure(TIMER2_BASE, TIMER_CFG_ONE_SHOT);
-    MAP_TimerLoadSet(TIMER0_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32DelayShutter - 228);
-    MAP_TimerLoadSet(TIMER1_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32DelayGasJet - 263);
-    MAP_TimerLoadSet(TIMER2_BASE, TIMER_A, MAP_SysCtlClockGet() / 1000 * g_ui32DelayShotCameras - 291);
+    MAP_TimerConfigure(TIMER3_BASE, TIMER_CFG_ONE_SHOT);
+    MAP_TimerConfigure(TIMER4_BASE, TIMER_CFG_ONE_SHOT);
+    MAP_TimerConfigure(TIMER5_BASE, TIMER_CFG_ONE_SHOT);
 
     // Setup the interrupts for the timer timeouts.
     MAP_IntEnable(INT_TIMER0A);
     MAP_IntEnable(INT_TIMER1A);
     MAP_IntEnable(INT_TIMER2A);
+    MAP_IntEnable(INT_TIMER3A);
+    MAP_IntEnable(INT_TIMER4A);
+    MAP_IntEnable(INT_TIMER5A);
     MAP_TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
     MAP_TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
     MAP_TimerIntEnable(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
+    MAP_TimerIntEnable(TIMER3_BASE, TIMER_TIMA_TIMEOUT);
+    MAP_TimerIntEnable(TIMER4_BASE, TIMER_TIMA_TIMEOUT);
+    MAP_TimerIntEnable(TIMER5_BASE, TIMER_TIMA_TIMEOUT);
 }
 
 //*****************************************************************************
@@ -145,9 +171,11 @@ void Timer0IntHandler(void) {
     // Clear the timer interrupt.
     MAP_TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 
-    // Set the PE3, PE4, PE5 back to low
+    // Set some pin value here
     MAP_GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 0x8);
-
+    MAP_GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0x4);
+    MAP_GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_1, 0x2);
+    MAP_GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0x40);
 }
 
 //*****************************************************************************
@@ -157,8 +185,10 @@ void Timer1IntHandler(void) {
     // Clear the timer interrupt.
     MAP_TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
 
-    // Set the PE3, PE4, PE5 back to low
+    // Set some pin value here
     MAP_GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_4, 0x10);
+    MAP_GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 0x8);
+    MAP_GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4, 0x10);
 }
 
 //*****************************************************************************
@@ -168,8 +198,40 @@ void Timer2IntHandler(void) {
     // Clear the timer interrupt.
     MAP_TimerIntClear(TIMER2_BASE, TIMER_TIMA_TIMEOUT);
 
-    // Set the PE3, PE4, PE5 back to low
+    // Set some pin value here
     MAP_GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_5, 0x20);
+    MAP_GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_0, 0x1);
+    MAP_GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_5, 0x20);
+}
+
+//*****************************************************************************
+// The interrupt handler for TIMER3
+//*****************************************************************************
+void Timer3IntHandler(void) {
+    // Clear the timer interrupt.
+    MAP_TimerIntClear(TIMER3_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Set some pin value here
+}
+
+//*****************************************************************************
+// The interrupt handler for TIMER4
+//*****************************************************************************
+void Timer4IntHandler(void) {
+    // Clear the timer interrupt.
+    MAP_TimerIntClear(TIMER4_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Set some pin value here
+}
+
+//*****************************************************************************
+// The interrupt handler for TIMER5
+//*****************************************************************************
+void Timer5IntHandler(void) {
+    // Clear the timer interrupt.
+    MAP_TimerIntClear(TIMER5_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Set some pin value here
 }
 
 void I10ToA(uint32_t value, char* result) {
@@ -208,36 +270,74 @@ void UARTSend(const char *pui8Buffer, uint32_t ui32Count) {
 //*****************************************************************************
 // Handle UART commands
 //*****************************************************************************
+void ReadDelay() {
+    char returnValue[12] = "          \012\015";
+
+    if (g_ui32UARTCommand[1] == '0') {
+        I10ToA(g_ui32Delay0, returnValue);
+    } else if (g_ui32UARTCommand[1] == '1') {
+        I10ToA(g_ui32Delay1, returnValue);
+    } else if (g_ui32UARTCommand[1] == '2') {
+        I10ToA(g_ui32Delay2, returnValue);
+    } else if (g_ui32UARTCommand[1] == '3') {
+        I10ToA(g_ui32Delay3, returnValue);
+    } else if (g_ui32UARTCommand[1] == '4') {
+        I10ToA(g_ui32Delay4, returnValue);
+    } else if (g_ui32UARTCommand[1] == '5') {
+        I10ToA(g_ui32Delay5, returnValue);
+    } else {}
+    UARTSend(returnValue, 12);
+}
+
+void WriteDelay() {uint8_t i = 0;
+    char returnValue[12] = "          \012\015";
+    char inputValue[5] = "     ";
+
+    for (i = 0; i < 5; i++) inputValue[i] = (char) g_ui32UARTCommand[i + 3];
+    if (g_ui32UARTCommand[1] == '0') {
+        g_ui32Delay0 = atoi(inputValue);
+        I10ToA(g_ui32Delay0, returnValue);
+    } else if (g_ui32UARTCommand[1] == '1') {
+        g_ui32Delay1 = atoi(inputValue);
+        I10ToA(g_ui32Delay1, returnValue);
+    } else if (g_ui32UARTCommand[1] == '2') {
+        g_ui32Delay2 = atoi(inputValue);
+        I10ToA(g_ui32Delay2, returnValue);
+    } else if (g_ui32UARTCommand[1] == '3') {
+        g_ui32Delay3 = atoi(inputValue);
+        I10ToA(g_ui32Delay3, returnValue);
+    } else if (g_ui32UARTCommand[1] == '4') {
+        g_ui32Delay4 = atoi(inputValue);
+        I10ToA(g_ui32Delay4, returnValue);
+    } else if (g_ui32UARTCommand[1] == '5') {
+        g_ui32Delay5 = atoi(inputValue);
+        I10ToA(g_ui32Delay5, returnValue);
+    } else {}
+    UARTSend(returnValue, 12);
+}
+
+void PauseTrigger() {
+    g_ui32RunningState = false;
+    MAP_GPIOIntDisable(GPIO_PORTE_BASE, GPIO_INT_PIN_1);
+}
+
+void UnpauseTrigger() {
+    g_ui32RunningState = true;
+    MAP_GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_1);
+}
+
 void HandleCommand() {
     MAP_UARTCharPutNonBlocking(UART0_BASE, 10);
     MAP_UARTCharPutNonBlocking(UART0_BASE, 13);
 
-
-    char returnValue[12] = "          \012\015";
-    char inputValue[5] = "     ";
     if (g_ui32UARTCommand[0] == 'R') {
-        if (g_ui32UARTCommand[1] == '1') {
-            I10ToA(g_ui32DelayShutter, returnValue);
-        } else if (g_ui32UARTCommand[1] == '2') {
-            I10ToA(g_ui32DelayGasJet, returnValue);
-        } else if (g_ui32UARTCommand[1] == '3') {
-            I10ToA(g_ui32DelayShotCameras, returnValue);
-        } else {}
-        UARTSend(returnValue, 12);
+        ReadDelay();
     } else if (g_ui32UARTCommand[0] == 'W') {
-        uint8_t i = 0;
-        for (i = 0; i < 5; i++) inputValue[i] = (char) g_ui32UARTCommand[i + 3];
-        if (g_ui32UARTCommand[1] == '1') {
-            g_ui32DelayShutter = atoi(inputValue);
-            I10ToA(g_ui32DelayShutter, returnValue);
-        } else if (g_ui32UARTCommand[1] == '2') {
-            g_ui32DelayGasJet = atoi(inputValue);
-            I10ToA(g_ui32DelayGasJet, returnValue);
-        } else if (g_ui32UARTCommand[1] == '3') {
-            g_ui32DelayShotCameras = atoi(inputValue);
-            I10ToA(g_ui32DelayShotCameras, returnValue);
-        } else {}
-        UARTSend(returnValue, 12);
+        WriteDelay();
+    } else if (g_ui32UARTCommand[0] == 'P' && g_ui32RunningState) {
+        PauseTrigger();
+    } else if (g_ui32UARTCommand[0] == 'S' && !g_ui32RunningState) {
+        UnpauseTrigger();
     }
 
     ResetUARTCommand();
@@ -247,17 +347,14 @@ void HandleCommand() {
 // The UART interrupt handler.
 //*****************************************************************************
 void UARTIntHandler(void) {
-    uint32_t ui32Status;
-
     // Get the interrupt status.
-    ui32Status = MAP_UARTIntStatus(UART0_BASE, true);
+    uint32_t ui32Status = MAP_UARTIntStatus(UART0_BASE, true);
 
     // Clear the asserted interrupts.
     MAP_UARTIntClear(UART0_BASE, ui32Status);
 
     // Loop while there are characters in the receive FIFO.
     while (MAP_UARTCharsAvail(UART0_BASE)) {
-        // Read the next character from the UART and write it back to the UART.
         uint32_t received = MAP_UARTCharGetNonBlocking(UART0_BASE);
         if (received != 13) {
             MAP_UARTCharPutNonBlocking(UART0_BASE, received);
@@ -268,11 +365,6 @@ void UARTIntHandler(void) {
         } else {
             HandleCommand();
         }
-
-        // Blink the LED to show a character transfer is occurring.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-        SysCtlDelay(SysCtlClockGet() / (10000 * 3));
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
     }
 }
 
@@ -304,15 +396,18 @@ void ConfigureUART() {
 // Main 'C' Language entry point.
 //*****************************************************************************
 int main(void) {
-    g_ui32DelayShutter = 80;
-    g_ui32DelayGasJet = 90;
-    g_ui32DelayShotCameras = 95;
-
+    g_ui32Delay0 = 70;
+    g_ui32Delay1 = 75;
+    g_ui32Delay2 = 80;
+    g_ui32Delay3 = 85;
+    g_ui32Delay4 = 90;
+    g_ui32Delay5 = 95;
+    g_ui32RunningState = true;
     ResetUARTCommand();
+
     ConfigureSystemClock();
     ConfigureTimer();
-    ConfigureIO_PortE();
-    ConfigureIO_PortF();
+    ConfigureIOPorts();
     ConfigureUART();
 
     while (1) {}
--- startup_ccs.c
+++ startup_ccs.c
@@ -53,6 +53,9 @@ extern void PortEIntHandler(void);
 extern void Timer0IntHandler(void);
 extern void Timer1IntHandler(void);
 extern void Timer2IntHandler(void);
+extern void Timer3IntHandler(void);
+extern void Timer4IntHandler(void);
+extern void Timer5IntHandler(void);
 extern void UARTIntHandler(void);
 
 //*****************************************************************************
@@ -124,7 +127,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port H
     IntDefaultHandler,                      // UART2 Rx and Tx
     IntDefaultHandler,                      // SSI1 Rx and Tx
-    IntDefaultHandler,                      // Timer 3 subtimer A
+    Timer3IntHandler,                       // Timer 3 subtimer A
     IntDefaultHandler,                      // Timer 3 subtimer B
     IntDefaultHandler,                      // I2C1 Master and Slave
     IntDefaultHandler,                      // Quadrature Encoder 1
@@ -159,7 +162,7 @@ void (* const g_pfnVectors[])(void) =
     0,                                      // Reserved
     IntDefaultHandler,                      // I2C2 Master and Slave
     IntDefaultHandler,                      // I2C3 Master and Slave
-    IntDefaultHandler,                      // Timer 4 subtimer A
+    Timer4IntHandler,                       // Timer 4 subtimer A
     IntDefaultHandler,                      // Timer 4 subtimer B
     0,                                      // Reserved
     0,                                      // Reserved
@@ -181,7 +184,7 @@ void (* const g_pfnVectors[])(void) =
     0,                                      // Reserved
     0,                                      // Reserved
     0,                                      // Reserved
-    IntDefaultHandler,                      // Timer 5 subtimer A
+    Timer5IntHandler,                       // Timer 5 subtimer A
     IntDefaultHandler,                      // Timer 5 subtimer B
     IntDefaultHandler,                      // Wide Timer 0 subtimer A
     IntDefaultHandler,                      // Wide Timer 0 subtimer B
